078dd29b0dd04fbbdd289e2b3b7b76e1ae04918c
Matthieu Bergel <matthieu@ourworldindata.org>
2024-01-26 20:51:45 +0000

    fix(baker): init connection

--- baker/SiteBaker.tsx
+++ baker/SiteBaker.tsx
@@ -391,6 +391,9 @@ export class SiteBaker {
 
     private async removeDeletedPosts() {
         if (!this.bakeSteps.has("removeDeletedPosts")) return
+
+        await db.getConnection()
+
         const postsApi = await getPostsFromSnapshots()
 
         const postSlugs = []
