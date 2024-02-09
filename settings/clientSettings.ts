3e930f70880405c9f3f0576275ca0856bfa7f2e4
Lars Yencken <lars@yencken.org>
2024-02-09 16:11:57 +0000

    :bug: fix defaults so that admin editing can run locally

--- settings/clientSettings.ts
+++ settings/clientSettings.ts
@@ -75,16 +75,18 @@ export const IMAGE_HOSTING_BUCKET_SUBFOLDER_PATH: string =
 
 // Production ETL API runs on http://etl-prod-2:8083/v1 (you need Tailscale to access it)
 export const ETL_API_URL: string =
-    process.env.ETL_API_URL ?? `${ADMIN_BASE_URL}:8081/api/v1`
+    process.env.ETL_API_URL ?? `http://${ADMIN_SERVER_HOST}:8081/api/v1`
 
 export const FASTTRACK_URL: string =
-    process.env.FASTTRACK_URL ?? `${ADMIN_BASE_URL}:8082/`
+    process.env.FASTTRACK_URL ?? `http://${ADMIN_SERVER_HOST}:8082/`
+
+console.log(`FASTTRACK_URL: ${FASTTRACK_URL}`)
 
 export const STAGING_SYNC_URL: string =
-    process.env.STAGING_SYNC_URL ?? `${ADMIN_BASE_URL}:8083/`
+    process.env.STAGING_SYNC_URL ?? `http://${ADMIN_SERVER_HOST}:8083/`
 
 export const WIZARD_URL: string =
-    process.env.WIZARD_URL ?? `${ADMIN_BASE_URL}:8053/`
+    process.env.WIZARD_URL ?? `http://${ADMIN_SERVER_HOST}:8053/`
 
 export const GDOCS_DETAILS_ON_DEMAND_ID: string =
     process.env.GDOCS_DETAILS_ON_DEMAND_ID ?? ""
